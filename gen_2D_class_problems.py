e6ee83f28244898a049ed9e8d3aeb8871dfa2f24
ReneLoh <r.lohmann@sms.ed.ac.uk>
2022-04-20 22:47:38 +0200

    Update
    Introduced variables Ntrain and Ntest to specify desired number of data points for both sets at the beginning of file.

--- gen_2D_class_problems.py
+++ gen_2D_class_problems.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 """Script to generate various 2-dimensional K-classification problems 
 to test novel training schemes on."""
 
-problem_type = "trigo"  # choose problem type to generate 
+problem_type = "spiral"  # choose problem type to generate 
                         # until now, allowed is "trigo" for a trigonometric data set
                         # and "spiral" for Swiss Roll type spirals.
                         # details of problems need to be specified below.
@@ -18,9 +18,12 @@ if problem_type == "trigo":
     b = 10   # larger = more difficult (frequency of trigonometric curves)
     D = 2    # dimensionality
     K = 2    # number of classes
+    Ntrain = 500   # number of points per class in train set
+    Ntest = 100    # number of points per class in test set    
+    
     
     ## Create Train Set
-    N = 500                                 # number of points per class
+    N = Ntrain
     X = np.zeros((N*K,D))                   # feature matrix (each row = single example)
     y = np.zeros(N*K, dtype='uint8')        # class labels
     
@@ -37,7 +40,7 @@ if problem_type == "trigo":
     np.save("trig_train_labels.npy", y)
     
     ## Create Test Set
-    N = 500                                 # number of points per class
+    N = Ntest 
     X = np.zeros((N*K,D))                   # feature matrix (each row = single example)
     y = np.zeros(N*K, dtype='uint8')        # class labels
     
@@ -58,15 +61,17 @@ elif problem_type == "spiral":
     a = 2       # constants
     p = 1
     c = 0.02
-    b = 5       # larger = more difficult (number of spiral turns)
+    b = 4       # larger = more difficult (number of spiral turns)
     D = 2       # dimensionality
     K = 2       # number of classes
+    Ntrain = 1   # number of points per class in train set
+    Ntest = 100    # number of points per class in test set
     
-    ## Create Train Set
-    N = 500                                     # number of points per class
+    ## Create Train Set                                   
+    N = Ntrain
     X = np.zeros((N*K,D))                       # feature matrix (each row = single example)
     y = np.zeros(N*K, dtype='uint8')            # class labels
-    
+   
     for j in range(K):
         t = rnd.uniform(size=N)
         rand = rnd.normal(size=N)
@@ -79,8 +84,8 @@ elif problem_type == "spiral":
     np.save("spiral_train_features.npy", X)     # write train features and labels
     np.save("spiral_train_labels.npy", y)
     
-    ## Create Test Set
-    N = 100                                     # number of points per class
+    ## Create Test Set                       
+    N = Ntest
     X = np.zeros((N*K,D))                       # feature matrix (each row = single example)
     y = np.zeros(N*K, dtype='uint8')            # class labels
     
