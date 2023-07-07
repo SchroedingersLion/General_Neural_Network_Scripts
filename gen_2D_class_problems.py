fixed a bug concerning how image data is fed into the model.forward() function in the train routines. also fixed incorrect normalization of accuracies in the eval() function of the architectures.

gen_2D_class_problems.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 """Script to generate various 2-dimensional K-classification problems 
 to test novel training schemes on."""
 
-problem_type = "spiral"  # choose problem type to generate 
+problem_type = "trigo"  # choose problem type to generate 
                         # until now, allowed is "trigo" for a trigonometric data set
                         # and "spiral" for Swiss Roll type spirals.
                         # details of problems need to be specified below.
@@ -35,7 +35,6 @@ if problem_type == "trigo":
         X[N*j:N*(j+1)] = np.concatenate((x1,x2), axis=1)
         y[N*j:N*(j+1)] = j
     
-
     np.save("trig_train_features.npy", X)   # write train features and labels
     np.save("trig_train_labels.npy", y)
     

torch_network_architectures.py
@@ -28,8 +28,8 @@ class Net(nn.Module):
         x = self.lin1(x)
         x = torch.relu(x)
         x = self.lin2(x)
-        # output = F.log_softmax(x, dim=1)
-        output = torch.sigmoid(x)
+        output = F.log_softmax(x, dim=1)
+        # output = torch.sigmoid(x)
         return output
     
     def test(self, data_loader, criterion):                                     ## returns network output to given input.
@@ -38,16 +38,17 @@ class Net(nn.Module):
         accu = 0        
         with torch.no_grad():                                                   ## don't keep track of gradients.
             for (features, targets) in data_loader:                             ## iterate over batches.
-                output = self.forward(features)                                 ## get model output.
-                # output = self.forward(features.squeeze().
-                # view((features.shape[0],-1)))                                 ## for images.
+                # output = self.forward(features)                               ## get model output.
+                output = self.forward(features.squeeze().
+                view((features.shape[0],-1)))                                 ## for images.
                 
-                # loss += F.nll_loss(output, targets, reduction="sum")          ## add losses of all samples in batch.
-                # accu += np.count_nonzero( targets==torch.argmax(output, dim=1) ) 
-                loss += criterion(output, torch.unsqueeze(targets, dim=1))      ## the unsqueeze is necessary for F.binary_cross_entropy, 
-                                                                                ## change otherwise.   
-                accu += np.count_nonzero(torch.unsqueeze(targets, dim=1) == 
-                                         (output >= 0.5))/ len(targets)
+                loss += F.nll_loss(output, targets)          ## add losses of all samples in batch.
+                accu += np.count_nonzero( targets==torch.argmax(output, dim=1) )/len(targets) 
+                
+                # loss += criterion(output, torch.unsqueeze(targets, dim=1))      ## the unsqueeze is necessary for F.binary_cross_entropy, 
+                #                                                                 ## change otherwise.   
+                # accu += np.count_nonzero(torch.unsqueeze(targets, dim=1) == 
+                #                          (output >= 0.5))/ len(targets)
 
         return (loss, accu)
 
@@ -285,4 +286,3 @@ class ada_Net_molec(nn.Module):
                 l2_error += criterion(output, targets.unsqueeze(dim=1))  
  
         return torch.sqrt( 1/len(data_loader.dataset) * l2_error )
-     

torch_train_routines.py
@@ -5,7 +5,8 @@ import time
 def train_model_classi(model, optimizer, criterion, epochs, trainloader, scheduler=None, testloader=None, eval_freq=0):
     """
     Training routine for N-class classification problem. Expects log_softmax as network output and criterion nll_loss.
-    Design problem: criterion is hardcoded in model.test().
+    Design problems: criterion is hardcoded in model.test(). For image data, feature tensor needs to be squeezed
+    before feed-in to model.forward().
     """
     
     loss_train = np.zeros(epochs+1)
@@ -33,11 +34,16 @@ def train_model_classi(model, optimizer, criterion, epochs, trainloader, schedul
     for epoch in np.arange(1,epochs+1):
         print("Epoch ", epoch)
         for (batchidx, (features, targets)) in enumerate(trainloader): 
-            output = model(features)
+            
+            # output = model(features)
+            
+            output = model(features.squeeze().    # FOR IMAGES
+            view((features.shape[0],-1)))
+            
             optimizer.zero_grad()
             loss = criterion(output, targets)
             loss_train[epoch] += loss.detach()
-            # print(loss_train.type)
+
             _, predicted = torch.max(output,1)
             accu_train[epoch] += torch.sum(predicted == targets) / len(targets)
             
@@ -60,7 +66,8 @@ def train_model_classi(model, optimizer, criterion, epochs, trainloader, schedul
 def train_model_classi2(model, optimizer, criterion, epochs, trainloader, scheduler=None, testloader=None, eval_freq=0):
     """
     2-class classification problem. Currently expects single sigmoidal output and criterion binary cross entropy loss.
-    Design problem: criterion is hardcoded in model.test().
+    Design problems: criterion is hardcoded in model.test(). For image data, feature tensor needs to be squeezed
+    before feed-in to model.forward().
     """
     
     loss_train = np.zeros(epochs+1)
@@ -89,6 +96,9 @@ def train_model_classi2(model, optimizer, criterion, epochs, trainloader, schedu
         # print("Epoch ", epoch)
         for (batchidx, (features, targets)) in enumerate(trainloader):
             output = model(features)
+            # output = model(features.squeeze().    # FOR IMAGES
+            # view((features.shape[0],-1)))           
+            
             optimizer.zero_grad()
             loss = criterion(output, targets.unsqueeze(1))
             loss_train[epoch] += loss.detach()